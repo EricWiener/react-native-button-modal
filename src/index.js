cb7c24338e7c8f9c3a69af80e926844efc385026
Eric Wiener <ericwiener3@gmail.com>
2018-03-29 23:31:34 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { View, StyleSheet, Text,} from "react-native";
+import { Button } from "native-base";
+import Modal from "react-native-modal";
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+export default class ButtonModal extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        { //buttonStyle == 'rectangle'
+          (this.props.buttonType=='rectangle') ?
+            <Button
+              block
+              bordered
+              dark={this.props.allowClick}
+              light={!this.props.allowClick}
+              style={styles.nonNativeBaseformElement}
+              success={this.props.success}
+              onPress={this.props.allowClick ? this.props.showModal : null}
+            >
+              <Text style={{fontSize: this.props.initialTextSize}}>
+                {this.props.buttonMessage}
+              </Text>
+            </Button>
+          : //buttonStyle == 'add'
+          (this.props.buttonType=='icon') ?
+            <Button transparent onPress={this.props.showModal}>
+              <Icon name={this.props.iconName} style={{fontSize: this.props.iconSize}}/>
+            </Button>
+          : //buttonStyle != 'add' or 'rectangle'
+            <Text>ERROR: Please specify a correct buttonType</Text>
+        }
+
+        <Modal
+          isVisible={this.props.isVisible}
+          style={styles.bottomModal}
+          backdropOpacity={0.7}
+          onBackdropPress={this.props.allowClose ? this.props.hideModal : null}
+          onSwipe={this.props.allowClose ? this.props.hideModal : null}
+          swipeDirection="down"
+        >
+          {this.props.content}
+          <Button block primary={this.props.allowClose} light={!this.props.allowClose} onPress={this.props.hideModal}>
+            <Text style={{fontSize: this.props.confirmTextSize}}> Confirm </Text>
+          </Button>
+        </Modal>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  bottomModal: {
+    justifyContent: "flex-end",
+    margin: 0
+  },
+  nonNativeBaseformElement: {
+    flex: 1,
+    marginLeft: 15,
+    marginRight: 15,
+    marginTop: 13
+  }
+});
+
+ButtonModal.defaultProps = {
+  buttonType: 'rectangle',
+  iconSize: 27,
+  confirmTextSize: 15,
+  buttonTextSize: 15,
+  iconName: 'add',
+  allowClose: true,
+  allowClick: true,
+  buttonMessage: 'Click here',
+}
